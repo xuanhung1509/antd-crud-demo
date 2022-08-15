06d8c6d08720791b335b83e68c9b8067e02025b5
xuanhung1509 <vuxuanhung15092002@gmail.com>
2022-08-15 15:34:50 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,30 @@
+import { useState } from 'react';
+import { DatePicker, message, Alert } from 'antd';
+
 function App() {
-  return <div>App</div>;
+  const [date, setDate] = useState(null);
+  const handleChange = (value) => {
+    message.info(
+      `Selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`
+    );
+    setDate(value);
+  };
+
+  return (
+    <div
+      style={{
+        width: 400,
+        margin: '100px auto',
+      }}
+    >
+      <DatePicker onChange={handleChange} />
+      <div style={{ marginTop: 16 }}>
+        <Alert
+          message='Selected Date'
+          description={date ? date.format('YYYY-MM-DD') : 'None'}
+        />
+      </div>
+    </div>
+  );
 }
 export default App;
